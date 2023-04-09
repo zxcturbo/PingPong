f41eda07de2263d3fdcc41b2c3ebc8c1ec460eb2
zxcturbo <128979491+zxcturbo@users.noreply.github.com>
2023-04-09 16:04:40 +0300

    добавил картинки

--- pingpong/main.py
+++ pingpong/main.py
@@ -8,7 +8,7 @@ FPS = 60
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_speed, player_x, player_y):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (60,60))
+        self.image = transform.scale(image.load(player_image), (600,100))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -25,16 +25,44 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys_pressed[K_DOWN] and self.rect.y < 410:
             self.rect.y += self.speed
-        if keys_pressed[K_LEFT] and self.rect.x > 3:
-            self.rect.x -= self.speed
-        if keys_pressed[K_RIGHT] and self.rect.x < 630:
-            self.rect.x += self.speed
+
+
+class Player2(GameSprite):
+    def __init__(self, player_image, player_speed, player_x, player_y):
+        super().__init__(player_image, player_speed, player_x, player_y)
+    def update(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_w] and self.rect.y > 3:
+            self.rect.y -= self.speed
+        if keys_pressed[K_s] and self.rect.y < 410:
+            self.rect.y += self.speed
+
+
+background = transform.scale(image.load('fon.jpg'), (700,500))
 
 game = True
+finish = False
+
+player1 = Player("palka.png", 10, 345, 200)
+player2 = Player2("palka.png", 10, 340, 200)
+
+
+font.init()
+font = font.SysFont('Arial', 70)
+win = font.render('YOU WIN!', True, (255,215,0))
+lose = font.render("YOU LOSE!", True, (255,0,0))
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-        clock.tick(FPS)
-        display.update()
+    if finish != True:
+        window.blit(background, (0,0))
+        player1.reset()
+        player1.update()
+
+        player2.reset()
+        player2.update()
+
+    clock.tick(FPS)
+    display.update()
